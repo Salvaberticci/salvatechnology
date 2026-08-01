fabb5824961d5e69c3458e60eefd35fb9d1dfbcd
Salvatore Berticci <salvatoreberticci19@gmail.com>
2026-08-01 11:34:27 -0400

    fix: add missing app.php require for BASE_URL in actividad_entregar

--- actividad_entregar.php
+++ actividad_entregar.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/config/db.php';
+require_once __DIR__ . '/config/app.php';
 
 if (!isset($_SESSION['usuario_id']) || $_SESSION['usuario_rol'] !== 'estudiante') {
     header('Location: academia');
