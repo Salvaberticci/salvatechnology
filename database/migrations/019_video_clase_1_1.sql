97a7f0485f483ba7c60f8f3a3660c496da42416d
Salvatore Berticci <salvatoreberticci19@gmail.com>
2026-08-06 14:48:35 -0400

    video: link Bunny Stream de la Clase 1.1 (migración 019)

---
+++ database/migrations/019_video_clase_1_1.sql
@@ -0,0 +1,2 @@
+-- Setea el video_url de la Clase 1.1 (El Arte de la Ingeniería: Dato, Información y Sistema).
+UPDATE lecciones SET video_url = 'https://player.mediadelivery.net/play/717430/a960b9ef-8f03-4e54-a817-babb05d71916' WHERE id = 1115;
